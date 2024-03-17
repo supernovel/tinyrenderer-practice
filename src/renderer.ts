9dbf679772fbca8d69c3c4cfbb15e072071a6270
supernovel <ekfueh@naver.com>
2024-03-17 12:55:14 +0000

    lesson1. third attemp

--- src/renderer.ts
+++ src/renderer.ts
@@ -8,9 +8,36 @@ export function line(
   image: Bitmap,
   color: BitmapColor,
 ) {
+  let steep = false;
+
+  // 변화량이 큰 방향으로 그리기
+  if (Math.abs(x0 - x1) < Math.abs(y0 - y1)) {
+    const temp = [x0, y0, x1, y1];
+    x0 = temp[1];
+    y0 = temp[0];
+    x1 = temp[3];
+    y1 = temp[2];
+
+    steep = true;
+  }
+
+  // 왼쪽에서 오른쪽으로 그리기
+  if (x0 > x1) {
+    const temp = [x0, y0, x1, y1];
+    x1 = temp[0];
+    y1 = temp[1];
+    x0 = temp[2];
+    y0 = temp[3];
+  }
+
   for (let x = x0; x < x1; x++) {
     const t = (x - x0) / (x1 - x0);
     const y = y0 + (y1 - y0) * t;
+
+    if (steep) {
+      image.set(y, x, color);
+      continue;
+    }
 
     image.set(x, y, color);
   }
